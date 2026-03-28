19e03ecebcaba67f8d206c469839f57abdd5068c
alexgit <resiga.alex@yahoo.com>
2026-03-29 00:07:26 +0200

    added MET property

--- VibeCoders.Core/Models/LoggedExercise.cs
+++ VibeCoders.Core/Models/LoggedExercise.cs
@@ -8,5 +8,6 @@ public sealed class LoggedExercise
     public int Id { get; set; }
     public string ExerciseName { get; set; } = string.Empty;
     public int WorkoutLogId { get; set; }
+    public float Met { get; set; }
     public List<LoggedSet> Sets { get; set; } = new();
 }
