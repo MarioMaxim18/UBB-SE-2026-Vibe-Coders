935ab7d9477b63ef2c4e734a3a90dd9abc82ee41
Alawerga <adrianfdez.rdez@gmail.com>
2026-03-28 20:45:33 +0200

    The 'NutritionPlan' class specified on the diagram

---
+++ VibeCoders/NutritionPlan.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class NutritionPlan
+{
+    //Atributes
+    private int _planId;
+    private string _startDate;
+    private string _endDate;
+    private List<Meal> _meals;
+
+    //Constructor
+    public NutritionPlan()
+    {
+        _meals = new List<Meal>();
+    }
+
+    //Getters and Setters
+    public int PlanId
+    {
+        get { return _planId; }
+        set { _planId = value; }
+    }
+
+    public string StartDate
+    {
+        get { return _startDate; }
+        set { _startDate = value; }
+    }
+
+    public string EndDate
+    {
+        get { return _endDate; }
+        set { _endDate = value; }
+    }
+
+    public List<Meal> Meals
+    {
+        get { return _meals; }
+        set { _meals = value; }
+    }
+
+    
+}
