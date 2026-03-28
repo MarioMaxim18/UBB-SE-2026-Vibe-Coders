caa3fd49ca51389768733f311a5d4c978a1f3ba2
Vlad Florin-Alexandru <vladfalex@gmail.com>
2026-03-29 00:03:52 +0200

    feat: Implement TrainerService.getAssignedClient logic

---
+++ VibeCoders.Core/Services/TrainerService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VibeCoders.Models;
+
+namespace VibeCoders.Services
+{
+    public class TrainerService
+    {
+       
+        public IDataStorage dataStorage { get; set; }
+
+     
+        public TrainerService(IDataStorage storage)
+        {
+            dataStorage = storage;
+        }
+
+        
+        public List<Client> getAssignedClient(int trainerId)
+        {
+            
+            return dataStorage.getTrainerClient(trainerId);
+        }
+
+        
+        public void assignWorkout(Client client, WorkoutLog workout)
+        {
+            throw new NotImplementedException("Workout assignment coming in Slice 2!");
+        }
+    }
+}
