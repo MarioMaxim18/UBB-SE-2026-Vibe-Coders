ee1900925199bf9fc88f065671613ef71be86700
Alex Mărcuș <67654354+AlexMarcus17@users.noreply.github.com>
2026-03-29 20:28:18 +0300

    Schedule Configuration UI Layout (#205)

---
+++ VibeCoders/Views/CalendarIntegrationPage.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace VibeCoders.Views
+{
+    public sealed partial class CalendarIntegrationPage : Page
+    {
+        public CalendarIntegrationPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
