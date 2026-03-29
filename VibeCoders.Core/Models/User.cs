17246bc682299cbbe03b45253819a29165f8f64a
Vlad Florin-Alexandru <vladfalex@gmail.com>
2026-03-29 12:32:41 +0300

    initialized non-nullable referance properties

--- VibeCoders.Core/Models/User.cs
+++ VibeCoders.Core/Models/User.cs
@@ -9,7 +9,7 @@ namespace VibeCoders.Models
     public class User
     {
         public int Id { get; set; }
-        public string Username { get; set; }
-        public string PasswordHash { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
     }
 }
