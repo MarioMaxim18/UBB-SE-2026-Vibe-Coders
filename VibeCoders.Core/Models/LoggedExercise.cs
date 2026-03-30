98f95f99624226e098d2cc1e05dbbc29e13bcd11
Vlad Florin-Alexandru <vladfalex@gmail.com>
2026-03-30 20:38:12 +0300

    updated log exercise

--- VibeCoders.Core/Models/LoggedExercise.cs
+++ VibeCoders.Core/Models/LoggedExercise.cs
@@ -5,10 +5,15 @@ namespace VibeCoders.Models;
 /// </summary>
 public sealed class LoggedExercise
 {
+    //TODO: ATTRIBUTES NEED UPDATE, OR THE DIAGRAM
     public int Id { get; set; }
     public string ExerciseName { get; set; } = string.Empty;
     public int WorkoutLogId { get; set; }
     public float Met { get; set; }
-    public List<LoggedSet> Sets { get; set; } = new();
+    public List<LoggedSet> Sets { get; set; } = new List<LoggedSet>();
+    public List<string> MuscleGroups { get; set; } = new List<string>();
     public int ExerciseCaloriesBurned { get; set; }
+    public double? PerformanceRatio { get; set; }
+    public bool IsSystemAdjusted { get; set; }
+    public string? AdjustmentNote { get; set; }
 }
