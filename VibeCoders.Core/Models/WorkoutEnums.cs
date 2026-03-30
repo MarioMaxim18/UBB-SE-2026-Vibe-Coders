3e0b00644da501b48e86e21701c6e1b0d2f5f7d0
GasparGabriel2006 <www.gheza99miha@yahoo.com>
2026-03-30 19:47:20 +0300

    Everything that should have been added yesterday evening + sqlLite fix (#225)

---
+++ VibeCoders.Core/Models/WorkoutEnums.cs
@@ -0,0 +1,25 @@
+﻿namespace VibeCoders.Models
+{
+    /// <summary>
+    /// Muscle groups used to determine weight increment steps during progression.
+    /// </summary>
+    public enum MuscleGroup
+    {
+        CHEST,
+        BACK,
+        LEGS,
+        SHOULDERS,
+        ARMS,
+        CORE
+    }
+
+    /// <summary>
+    /// Describes the origin / ownership of a workout template.
+    /// </summary>
+    public enum WorkoutType
+    {
+        CUSTOM,
+        PREBUILT,
+        TRAINER_ASSIGNED
+    }
+}
