dbd5ff98659fe35140541c00c4c88fa4cdc4887e
Mario Maxim <maximmarioandrei@gmail.com>
2026-04-06 16:30:58 +0300

    fix

--- VibeCoders/Services/UserSession.cs
+++ VibeCoders/Services/UserSession.cs
@@ -2,7 +2,7 @@ namespace VibeCoders.Services;
 
 public sealed class UserSession : IUserSession
 {
-    public long CurrentUserId { get; set; } = 1;
+    public long CurrentUserId { get; set; } = 2;
 
     public long CurrentClientId { get; set; } = 1;
 }
